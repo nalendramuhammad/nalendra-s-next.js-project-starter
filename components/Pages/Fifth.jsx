e6a2b144d7f6ba3f60ad9b97faf080b10a6b9688
nalendramuhammad <itsnalendraa@gmail.com>
2024-03-06 14:05:13 +0700

    second commit

---
+++ components/Pages/Fifth.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "@/styles/Pages/Fifth.module.scss";
+
+export default function Fifth() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p>Fifth</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import First from "@/components/Pages/First";
 import Second from "@/components/Pages/Second";
 import Third from "@/components/Pages/Third";
 import Forth from "@/components/Pages/Forth";
+import Fifth from "@/components/Pages/Fifth";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <Second />
         <Third />
         <Forth />
+        <Fifth />
       </Layout>
     </main>
   );
